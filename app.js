498a03f83dcdb9afb389bfca2ef66aee5e5c08e0
Tewabe Tadege <ethiohabesha412@gmail.com>
2022-06-28 22:48:36 -0700

    Passport

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 
+const passport = require('passport');
+const authenticate = require('./authenticate');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
@@ -44,28 +47,26 @@ app.use(session({
   resave: false,
   store: new FileStore()
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 //this is where we will add authentication
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth(req, res, next) {
-    console.log(req.session);
 
-    if (!req.session.user) {
-        const err = new Error('You are not authenticated!');
+function auth(req, res, next) {
+    console.log(req.user);
+    
+    if (!req.user) {
+        const err = new Error('You are not authenticated!');                    
         err.status = 401;
         return next(err);
     } else {
-        if (req.session.user === 'authenticated') {
-            return next();
-        } else {
-            const err = new Error('You are not authenticated!');
-            err.status = 401;
-            return next(err);
-        }
+        return next();
     }
 }
+
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
