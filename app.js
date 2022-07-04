27636249bad12f29c297324e76e8d053d42c33ca
Tewabe Tadege <ethiohabesha412@gmail.com>
2022-07-04 03:32:37 -0700

    Uploading Files

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 
+const uploadRouter = require('./routes/uploadRouter');
+
 const mongoose = require('mongoose');
 
 const url = config.mongoUrl;
@@ -60,6 +62,8 @@ app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+//Multer 
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
